cab50415e94d9454e884fa4a9a0627d6abebafb3
helloyou2012 <helloyou2012@yeah.net>
2020-10-26 18:25:49 +0800

    version 1.0.1

--- src/Node.js
+++ src/Node.js
@@ -1,50 +1,60 @@
-import React, { useLayoutEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 export default function Node({
   onSize, children, top, left, className, prefixCls
 }) {
-  const ref = useRef(null);
+  const elem = useRef(null);
   const oldW = useRef(0);
   const oldH = useRef(0);
+  const observer = useRef(null);
 
-  useLayoutEffect(() => {
-    if (!ref.current) return () => {};
+  const onChange = () => {
+    if (!elem.current) return;
+    const rect = elem.current.getBoundingClientRect();
+    const width = Math.round(rect.width || 0);
+    const height = Math.round(rect.height || 0);
+    if (width !== oldW.current || height !== oldH.current) {
+      oldW.current = width;
+      oldH.current = height;
+      onSize({ width, height });
+    }
+  };
 
-    const onChange = () => {
-      const rect = ref.current.getBoundingClientRect();
-      const width = Math.round(rect.width || 0);
-      const height = Math.round(rect.height || 0);
-      if (width !== oldW.current || height !== oldH.current) {
-        oldW.current = width;
-        oldH.current = height;
-        onSize({ width, height });
-      }
-    };
-
-    document.addEventListener('transitionend', onChange);
-    window.addEventListener('resize', onChange);
-    let observer = null;
-    if (typeof MutationObserver !== 'undefined') {
-      observer = new MutationObserver(onChange);
-      observer.observe(ref.current, {
+  const addObserver = () => {
+    if (!elem.current && typeof MutationObserver !== 'undefined') {
+      observer.current = new MutationObserver(onChange);
+      observer.current.observe(elem.current, {
         attributes: true,
         childList: true,
         characterData: true,
         subtree: true
       });
     }
-    // call at first
+  };
+
+  const removeObserver = () => {
+    if (!observer.current) return;
+    observer.disconnect();
+  };
+
+  const ref = (node) => {
+    if (!node || node === elem.current) return;
+    removeObserver();
+    elem.current = node;
     onChange();
-    // release events
+    addObserver();
+  };
+
+  useEffect(() => {
+    document.addEventListener('transitionend', onChange);
+    window.addEventListener('resize', onChange);
     return () => {
       document.removeEventListener('transitionend', onChange);
       window.removeEventListener('resize', onChange);
-      if (observer) {
-        observer.disconnect();
-      }
+      removeObserver();
     };
-  }, [ref.current]);
+  }, []);
 
   const cls = [`${prefixCls}-node`];
   if (className) {
@@ -66,7 +76,7 @@ export default function Node({
 Node.displayName = 'Node';
 Node.propTypes = {
   onSize: PropTypes.func.isRequired,
-  children: PropTypes.element,
+  children: PropTypes.node,
   top: PropTypes.number,
   left: PropTypes.number,
   className: PropTypes.string,
